6ebbb0fd18bc438321e407b13b07ac2309fac078
gio <garm9609@gmail.com>
2022-11-08 16:45:16 -0600

    funciones de js

---
+++ tarea7/arrayapp.js
@@ -0,0 +1,11 @@
+let frutas = ["manzana","pera","sandia","mandarina"];
+console.log(frutas);
+
+for(let i = 0; i<frutas.length;i++){
+    console.log(frutas[i]);
+}
+
+for(let fruta of frutas){
+    console.log(fruta);
+
+}
---
+++ tarea7/funcion.js
@@ -0,0 +1,24 @@
+function hola(){
+    console.log("hola");
+}
+hola();
+
+let nombre = prompt("como te llamas?")
+function adios(textousuario){
+    console.log("adios "+textousuario);
+
+}
+adios(nombre);
+
+let numero1 = parseInt(prompt("cual es el  primer numero?"));
+
+let numero2 = parseInt(prompt("cual es el segundo numero?"));
+
+function sumar(n1,n2){
+    console.log("La suma de los numeros es: ",n1+n2);
+
+}
+
+sumar(numero1,numero2);
+
+
