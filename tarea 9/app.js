4cf97810228c81d2f6a6480ba128cd097ca2ee71
gio <garm9609@gmail.com>
2022-11-15 01:05:14 -0600

    ejercicios de clase y tareas

---
+++ tarea 9/app.js
@@ -0,0 +1,13 @@
+var lista = [];
+
+function insertar(){
+lista.push(prompt("Que desea agregar al carrito?"));
+}
+
+function quitar(){
+lista.pop();
+}
+
+function mostrar(){
+    console.log(lista);
+}
---
+++ tarea8/app.js
@@ -0,0 +1,18 @@
+// let n1 = parseInt(prompt("Introduzca el primer numero"));
+// let n2 = parseInt(prompt("Introduzca el segundo numero"));
+// var r;
+
+// function mult(num1,num2){
+//     for(i=0; i<=num2;i++){
+//         r=num1*i;
+//     console.log(num1+"*"+i+"="+r);
+// }
+// }
+
+// mult(n1,n2);
+
+// Plantillas literales
+let nombre = "gio";
+console.log(`Saludo: ${nombre.toUpperCase()}`);
+nombre = "jaky";
+console.log(`Saludo: ${nombre.toUpperCase()}`);
